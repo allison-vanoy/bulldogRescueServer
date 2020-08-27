3652865c677dd70924930ef178776340178302dc
Allison Vanoy <allison_vanoy@hotmail.com>
2020-08-26 20:35:02 -0400

    add operations to dogs and dogId endpoints

--- routes/dogRouter.js
+++ routes/dogRouter.js
@@ -13,7 +13,25 @@ dogRouter.route('/')
 			res.json(dogs);
 		})
 		.catch(err => next(err));
-	});
+	})
+	.post((req, res, next) => {
+		Dog.create(req.body)
+		.then(dog => {
+			console.log('dog created', dog);
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			res.json(dog);
+		})
+		.catch(err => next(err));
+	})
+	.put((req, res) => {
+		res.statusCode = 403;
+		res.end('PUT operation not supported on /dogs');
+	})
+	.delete((req, res) => {
+		res.statusCode = 403;
+		res.end('DELETE operation not supported on /dogs');
+	})
 
 dogRouter.route('/:dogId')
 	.get((req, res, next) => {
@@ -22,6 +40,30 @@ dogRouter.route('/:dogId')
 			res.statusCode = 200;
 			res.setHeader('Content-Type', 'application/json');
 			res.json(dog);
+		})
+		.catch(err => next(err));
+	})
+	.post((req, res) => {
+		res.statusCode = 403;
+		res.end(`POST operation not supported on /dogs/${req.params.dog}`);
+	})
+	.put((req, res, next) => {
+		Dog.findByIdAndUpdate(req.params.dogId, {
+			$set: req.body
+		}, {new: true})
+		.then(dog => {
+			res.statusCode = 200;
+			res.setHeader('COntent-Type', 'application/json');
+			res.json(dog);
+		})
+		.catch(err => next(err));
+	})
+	.delete((req, res, next) => {
+		Dog.findByIdAndDelete(req.params.dogId)
+		.then(response => {
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			res.json(response)
 		})
 		.catch(err => next(err));
 	});
